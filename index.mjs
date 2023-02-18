bce0dc11e8c4b71229d2f2a3488af49bd55880cf
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-18 18:50:05 +0000

    adding description inquirer

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+import fs from "fs/promises";
+
+
+let {description, size} = await inquirer
+    .prompt([
+      {
+            type: 'input',
+            name: 'description',
+            message: "write a description of your projetc:",
+        },
+        {
+            type: 'list',
+            name: 'size',
+            message: 'what size do you need?',
+            choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+            filter(val) {
+              return val.toLowerCase();
+         },
+       },
+    
+    ])
