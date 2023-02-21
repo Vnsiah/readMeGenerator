f27db55a40e5df972cd932fb05e6427b979c1edb
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-21 18:29:38 +0000

    creating Markdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,6 +6,47 @@ function generateReadMe(answers) {
   
 
 
+## Description
+ ${answers.description}
+
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+ ${answers.installation}
+
+
+## Usage
+${answers.usage}
+
+
+
+
+
+## Contributing
+ ${answers.contributing}
+
+
+## Tests
+✏️ ${answers.tests}
+
+
+## Questions
+Have questions about this project?  
+GitHub: https://github.com/${data.github}  
+Email: ${answers.email}
+
+
+_This README was generated with ❤️ by [readMeGenerator](https://github.com/Vnsiah/readMeGenerator)_
+
   
 
 `;
