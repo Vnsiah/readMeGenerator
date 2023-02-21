cd190ff17c429e0329aa8d5daaa2b06ac10b318b
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-02-21 18:57:54 +0000

    adding license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,33 @@
+// import fs from "fs/promises";
+import fs from require('fs');
+
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
+
+// Returns the license link
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+ 
+}
+
+// Returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+}
+
 // function to generate markdown for README
 
 function generateReadMe(answers) {
